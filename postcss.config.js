eaadb270a39275090f60de24863866cd46321bda
iuhoay <iuhoay@gmail.com>
2021-05-19 01:10:15 +0800

    CSS minify

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-nested'),
+    require('cssnano')({
+      preset: 'default',
+    }),
     require('tailwindcss'),
     require('autoprefixer'),
   ]
